1e2055f957c83f2282352458530400df44dd34ef
Kate Spitalnic <kespitalnic@gmail.com>
2022-02-17 15:09:51 -0500

    tag routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category 
+Product.belongsTo(Category, {
+  foreignKey: 'id'
+})
 
 // Categories have many Products (but product can only belong to one category)
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  // as: ('voted_posts')
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: Product_Tag, 
+  //as: 
+  foreignKey: 'product_id'
+})
 
 module.exports = {
   Product,
