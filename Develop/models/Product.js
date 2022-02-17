92bb2f4dff6f1787255edd56e2c31d09d17c2c33
Kate Spitalnic <kespitalnic@gmail.com>
2022-02-17 15:19:20 -0500

    add price and stock column to product.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -18,8 +18,20 @@ Product.init(
     product_name: {
       type: DataTypes.STRING, 
       allowNull: false,
+    },
+    price: {
+      type: (DataTypes.DECIMAL(10,2)),
+      allowNull: false,
       validate: {
         isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      defaultValue: 10, 
+      validate: {
+        isNumeric: true
       }
     },
     category_id: {
